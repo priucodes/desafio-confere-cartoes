9efb437c8d49e0dc4b3f1f443e950baf646749df
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2019-09-23 22:04:52 -0300

    feat: associate the Card mode and the User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,10 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.Card, { foreignKey: 'card_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
