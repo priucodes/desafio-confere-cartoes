2dff89e8837d194a7d94b177de71ec691af71e53
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2019-09-23 21:49:30 -0300

    feat: create card controller

---
+++ src/app/controllers/CardController.js
@@ -0,0 +1,27 @@
+import Card from '../models/Card';
+
+/**
+ * TODO:
+ * - [] Save and return only the last 4 card digits
+ * - [] Fix the date format for the expiry
+ * - [] Validate the card number size
+ * - [] Validate the card cvv size
+ */
+
+class CardController {
+  async store(req, res) {
+    const {
+      id, number, expiry, cvv, holder,
+    } = await Card.create(req.body);
+
+    return res.json({
+      id,
+      number,
+      expiry,
+      cvv,
+      holder,
+    });
+  }
+}
+
+export default new CardController();
