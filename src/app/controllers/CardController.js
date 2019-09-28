6fa476408ae7853d5bd97cc4a8cae4f2291402ca
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2019-09-28 19:24:19 -0300

    feat: card validation

--- src/app/controllers/CardController.js
+++ src/app/controllers/CardController.js
@@ -1,19 +1,37 @@
 import { format } from 'date-fns';
+import * as Yup from 'yup';
 import Card from '../models/Card';
 
 /**
  * TODO:
  * - [x] Save and return only the last 4 card digits
  * - [x] Fix the date format for the expiry
- * - [] Validate the card number size
- * - [] Validate the card cvv size
+ * - [] Validate the minimum card cvv size
+ * - [] Validate the minimum card number size
  *
  * OBSERVATIONS:
  * - The card can be used by various users
+ * - For now, it's needed to insert the full date - fix later
  */
 
 class CardController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      number: Yup.number()
+        .required()
+        .positive(),
+      expiry: Yup.date().required(),
+      cvv: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      holder: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails! Check the card information again.' });
+    }
+
     const {
       id, number, expiry, cvv, holder,
     } = await Card.create(req.body);
